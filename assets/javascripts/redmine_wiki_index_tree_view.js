86d3cd4f1dbf0358e26ea1925b61959f202c2b55
basyura <basyura@gmail.com>
2014-04-29 21:56:12 +0900

    added keyword filter

--- assets/javascripts/redmine_wiki_index_tree_view.js
+++ assets/javascripts/redmine_wiki_index_tree_view.js
@@ -1,9 +1,45 @@
 
 $(document).ready(function(){
+  // changed to treeview
 	$(".pages-hierarchy:first").treeview({
 		collapsed: true,
 	});
+  // expand first node
   $(".hitarea:first").click();
   // open second hierarchy
-  //$('.treeview:first').find('ul:first').children('li').find('.hitarea:first').click();
+  $('.treeview:first').find('ul:first').children('li').find('.hitarea:first').click();
+  // add keyword textbox
+  var filter_keyword = $('<input type="text" value="" id="filter_keyword">');
+  $('h2').after(filter_keyword);
+
+  // expand tree
+  $('#filter_keyword').focus(function() {
+    $('.pages-hierarchy:first').find(".expandable-hitarea").each(function() {
+        $(this).click();
+    });
+  });
+  // filter by keyword
+  $('#filter_keyword').keyup(function() {
+    var text = filter_keyword.val();
+    if (text == '') {
+      $('.pages-hierarchy:first').find("li").each(function() { $(this).css('display', ''); });
+      return;
+    }
+    // filter regexp
+    var regex = new RegExp(text, 'i');
+
+    $('.pages-hierarchy:first').find("a").each(function() {
+        var title = $(this).text();
+        if (title.match(regex)) {
+          var pare = $(this).parent('li')
+          while (pare.size() != 0) {
+            pare.css('display', '');
+            pare = pare.parent().parent('li');
+          }
+        }
+        else {
+          $(this).parent('li').css('display', 'none');
+        }
+    });
+  });
 });
